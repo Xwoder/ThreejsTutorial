62822c63acef9cee7a8d7cce59b7bcca4f62ba3e
Xwoder <Xwoder@qq.com>
2026-07-30 14:04:42 +0800

    refactor(geometry): 合并多面体课程为统一入口

--- src/lessons/geometry/builtinGeometries.ts
+++ src/lessons/geometry/builtinGeometries.ts
@@ -6,17 +6,13 @@ import { torusGeometry } from './torusGeometry';
 import { torusKnotGeometry } from './torusKnotGeometry';
 import { cylinderGeometry } from './cylinderGeometry';
 import { circleGeometry } from './circleGeometry';
-import { dodecahedronGeometry } from './dodecahedronGeometry';
 import { capsuleGeometry } from './capsuleGeometry';
 import { latheGeometry } from './latheGeometry';
 import { extrudeGeometry } from './extrudeGeometry';
-import { icosahedronGeometry } from './icosahedronGeometry';
-import { octahedronGeometry } from './octahedronGeometry';
 import { planeGeometry } from './planeGeometry';
-import { polyhedronGeometry } from './polyhedronGeometry';
+import { polyhedron } from './polyhedronLesson';
 import { ringGeometry } from './ringGeometry';
 import { shapeGeometry } from './shapeGeometry';
-import { tetrahedronGeometry } from './tetrahedronGeometry';
 import { tubeGeometry } from './tubeGeometry';
 import { edgesGeometry } from './edgesGeometry';
 import { wireframeGeometry } from './wireframeGeometry';
@@ -29,17 +25,13 @@ export const builtinGeometries: Lesson[] = [
   torusKnotGeometry,
   cylinderGeometry,
   circleGeometry,
-  dodecahedronGeometry,
   capsuleGeometry,
   latheGeometry,
   extrudeGeometry,
-  icosahedronGeometry,
-  octahedronGeometry,
   planeGeometry,
-  polyhedronGeometry,
+  polyhedron,
   ringGeometry,
   shapeGeometry,
-  tetrahedronGeometry,
   tubeGeometry,
   edgesGeometry,
   wireframeGeometry,
